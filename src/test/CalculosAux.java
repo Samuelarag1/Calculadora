eac6ffe07012b7ef1a557d71beb10d18f916b202
Samuel Aragon <samuelaragon616@gmail.com>
2023-07-10 12:49:01 -0300

    Funcion de suma realizada

---
+++ src/test/CalculosAux.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class CalculosAux {
+    public static void main(String[] args) {
+        
+        Double suma = 0.0;
+        
+        ArrayList<Double> numeros = new ArrayList<Double>();
+        
+       
+        numeros.add(2.5);
+        numeros.add(2.4);
+        numeros.add(2.3);
+        numeros.add(2.2);
+        numeros.add(2.1);
+        
+    
+    for(int i = 0; i < numeros.size(); i++){
+             if(i == 0){
+                suma = numeros.get(i);
+        }if(i != 0){
+            suma += numeros.get(i);
+        }
+    }
+        System.out.println(suma);
+        }
+}
